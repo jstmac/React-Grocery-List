1d1ca3b3f3b7bc212bcf3b4a8f9d82af9107a063
Melissa G <jstmac@gmail.com>
2019-06-03 17:43:45 -0600

    get input values completed

--- src/GroceryForm.js
+++ src/GroceryForm.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 class GroceryForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state= {name: ""}
+  }
+  
+  handleChange = (e) => {
+    this.setState({ name: e.target.value });
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.name)
+  }
+
   render() {
+    // const { name} = this.state;
+
     return (
-      <form>
-        <input required placeholder="Add An Item" />
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          value={this.state.name}
+          name="name"
+          onChange={this.handleChange}
+          required 
+          placeholder="Add An Item" 
+          />
       </form>
     )
   }
